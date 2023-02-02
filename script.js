2c3a31d9580df0be3bc46668230b185461b35ec5
Yash Sachan <yashsachan321@gmail.com>
2023-02-03 00:13:21 +0530

    form visible now ,not functional

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 navigator.geolocation.getCurrentPosition(
   function (position) {
     //destructuring object
@@ -18,29 +19,18 @@ navigator.geolocation.getCurrentPosition(
     console.log(latitude);
     console.log(longitude);
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
       console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidht: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Working')
-        .openPopup();
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   function () {
@@ -49,3 +39,26 @@ navigator.geolocation.getCurrentPosition(
     );
   }
 );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidht: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Working')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
